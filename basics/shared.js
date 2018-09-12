a2419ffd50164eb049db55e7a007c51e72ed35bd
the-markussohn <markus.justinas@gmail.com>
2018-09-12 23:57:36 +0300

    side navigation bar

--- basics/shared.js
+++ basics/shared.js
@@ -2,14 +2,24 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modalNoButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 selectPlanButtons.forEach(btn => btn.addEventListener('click', () => {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
 }));
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
 modalNoButton.addEventListener('click', closeModal);
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 function closeModal() {
     modal.style.display = 'none';
